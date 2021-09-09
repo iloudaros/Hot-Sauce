4be652f445f5912a48ba8ae0bcd5be05796c6b9d
Ioannis Loudaros <johnloudaros@hotmail.com>
2021-09-09 23:20:11 +0300

    Just cleaning up the example instead of crying

--- Documentation/Extras/Calculator/fb3-1.h
+++ Documentation/Extras/Calculator/fb3-1.h
@@ -1,13 +1,3 @@
-/* Companion source code for "flex & bison", published by O'Reilly
- * Media, ISBN 978-0-596-15597-1
- * Copyright (c) 2009, Taughannock Networks. All rights reserved.
- * See the README file for license conditions and contact info.
- * $Header: /home/johnl/flnb/code/RCS/fb3-1.h,v 2.1 2009/11/08 02:53:18 johnl Exp $
- */
-/*
- * Declarations for a calculator fb3-1
- */
-
 /* interface to the lexer */
 extern int yylineno; /* from lexer */
 void yyerror(char *s, ...);
@@ -33,4 +23,3 @@ double eval(struct ast *);
 
 /* delete and free an AST */
 void treefree(struct ast *);
-
