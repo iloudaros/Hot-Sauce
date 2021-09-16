dcccd33a5f2c9792b5cae3b3788cca27477d225b
itsikelis <tsikelis.i@protonmail.com>
2021-09-17 02:55:59 +0300

    Now printing bottle

---
+++ Code/exec_helper/print_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("          __ \n");
+  printf("         [__] \n");
+  printf("         |  | \n");
+  printf("         |  | \n");
+  printf("         |  | \n");
+  printf("         |  | \n");
+  printf("         |  | \n");
+  printf("       / -.  \\   \n");
+  printf("      /-._|   \\  \n");
+  printf("      | `      |   \n");
+  printf("      |-....   |   \n");
+  printf("      |\'HOT\'|  | \n");
+  printf("      |SAUCE|  |  \n");
+  printf("      |`XXX |  |  \n");
+  printf("      |        |  \n");
+  printf("      `-......-\' \n");
+
+  return 0;
+}
